3a5cb2ab5a5e258dc67e59cb6d12b190afa044a1
Sapdotten <semenova.a.p.04@gmail.com>
2024-09-10 23:30:39 +0400

    working variant

---
+++ main.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
+import logging
+import app
+from dotenv import load_dotenv
+import handlers
+import app
+
+logging.basicConfig(level=logging.INFO)
+def register_routers(dp):
+    dp.include_routers(handlers.router)
+
+
+    
+async def main() -> None:
+    """
+    Entry point
+    """
+    # load_environ()
+    load_dotenv()
+    session = AiohttpSession()
+    bot = Bot(os.getenv("BOT_TOKEN"), session=session)
+    dp = Dispatcher()
+    register_routers(dp)
+    try:
+        await bot.delete_webhook()
+        await  asyncio.create_task(dp.start_polling(bot))
+        
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
